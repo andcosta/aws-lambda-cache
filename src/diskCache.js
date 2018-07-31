9869cff6b6aa04924a19b6c3ce5ac9519e7a8efd
Anderson de Carvalho <anderson.analistati@gmail.com>
2018-07-30 21:28:45 -0300

    Files MemoryCache and DiskCache

---
+++ src/diskCache.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class DiskCache {
+  constructor() {
+    this._defaulExpiresAt = 5; // 5 Minutes.
+  }
+}
+
+module.exports = DiskCache;
--- src/index.js
+++ src/index.js
@@ -1,50 +1,6 @@
 'use strict';
 
-class MemoryCache {
-  constructor() {
-    this._store = Object.create(null);
-    this._defaulExpiresAt = 5; // 5 Minutes.
-  }
-
-  set(key, value, expiresAtMinutes) {
-    this._store[key] = {
-      expiresAt: new Date(new Date().getTime() + this._expiresAt(expiresAtMinutes) * 60000).getTime(),
-      payload: value
-    };
-  }
-
-  get(key) {
-    var item = this._store[key];
-
-    if (!item) return undefined;
-    if (new Date().getTime() >= item.expiresAt) {
-      this.remove(key);
-      return undefined;
-    }
-
-    return item.payload;
-  }
-
-  remove(key) {
-    delete this._store[key];
-  }
-
-  clear() {
-    this._store = Object.create(null);
-  }
-
-  _expiresAt(expiresAt) {
-    if (typeof expiresAt !== 'number') return this._defaulExpiresAt;
-    if (expiresAt <= 0) return this._defaulExpiresAt;
-
-    return expiresAt;
-  }
-}
-
-class DiskCache {
-  constructor() {
-    this._defaulExpiresAt = 5; // 5 Minutes.
-  }
-}
+var MemoryCache = require('./memoryCache');
+var DiskCache = require('./diskCache');
 
 module.exports = { MemoryCache, DiskCache };
